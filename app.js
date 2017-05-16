a3ae528f8af77067c20ea010334cb1776ecd6689
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-05-16 09:02:45 +0800

    (updata):add ejs engine,routers description:添加ejs模板引擎，添加api路由

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var ejs = require('ejs')
+
+//router setup
 var index = require('./routes/index');
-var users = require('./routes/users');
+var apis  =require('./routes/apis');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'www'));
+app.set('view engine', 'ejs');
+app.engine('html', ejs.__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +26,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//router usage setup
 app.use('/', index);
-app.use('/users', users);
+app.use('/api', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,14 +36,9 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
---
+++ routes/apis.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var userModel = require('../models/user')
+
+
+
+//form 表单提交
+// router.post('/',function(req,res){
+//   console.log('Name(from querystring):'+req.body.name)
+//   console.log('studentNum(from querystring):'+req.body.studentNum)
+//   console.log('sex(from querystring):'+req.body.sex)
+//   console.log('telephone(from querystring):'+req.body.telephone)
+//   console.log('file:'+req.body.files);
+//     userModel.find({telephone:req.body.telephone},function(err,user){
+//         if(user.length > 0){
+//             console.log(user.telephone);                
+//             res.send(401,{error:
+//                 {
+//                     msg:'telephone 重复',
+//                     userPhone:user.telephone,
+//                     reqPhone:req.body.telephone
+//                 }
+//             })
+//         }else{
+//             //存数据
+//             new userModel({
+//                 name:req.body.name,
+//                 studentNum:req.body.studentNum,
+//                 sex:req.body.sex,
+//                 telephone:req.body.telephone
+//             }).save();
+//             res.send({success:true})
+//         }
+//     });
+// })
+
+//ajax提交
+router.post('/',function(req,res){
+    console.log('Name(from querystring):'+req.body.name)
+    console.log('studentNum(from querystring):'+req.body.studentNum)
+    console.log('sex(from querystring):'+req.body.sex)
+    console.log('telephone(from querystring):'+req.body.telephone)
+    if(req.xhr || req.accepts('json,html')=='json'){
+        userModel.find({telephone:req.body.telephone},function(err,user){
+            if(user.length > 0){
+                console.log(user.telephone);                
+                res.send(401,{error:
+                    {
+                        msg:'telephone 重复',
+                        userPhone:user.telephone,
+                        reqPhone:req.body.telephone
+                    }
+                })
+            }else{
+                //存数据
+                new userModel({
+                    name:req.body.name,
+                    studentNum:req.body.studentNum,
+                    sex:req.body.sex,
+                    telephone:req.body.telephone
+                }).save();
+                res.send({success:true})
+            }
+        });
+    }else{
+        res.send(400,{error:'error'})
+    }
+})
+module.exports = router;
