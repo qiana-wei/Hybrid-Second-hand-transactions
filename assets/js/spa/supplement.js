605eeaee95cb2b3c5732de882de86ea3be7bbe7c
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-05-24 10:11:22 +0800

    (push):add supplement page

---
+++ assets/js/spa/supplement.js
@@ -0,0 +1,32 @@
+Vue.component('supplement', {
+    name: 'supplement',
+    template: getSupplementTemplate(),
+    data(){
+        return{
+            userInfo: this.$root.userInfo,
+            username:'',
+            admission_time:'',
+            sex:'',
+        }
+    },
+    mounted(){
+
+    },
+    methods: {
+        changeCurrentView(name){
+            this.$emit('event','changeCurrentView',name)
+        },
+        startUse(){
+            let url = `/api/user/info`
+            let data = {
+                username : this.username,
+                admission_time : this.admission_time,
+                sex : this.sex
+            }
+            data = $.extend(data,this.userInfo)
+            utils.DataService.patch(url,data).done(res=>{
+                console.log(res);
+            })
+        }
+    }
+})
