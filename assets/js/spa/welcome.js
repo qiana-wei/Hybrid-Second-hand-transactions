a554942e3675dbd5ba7a619872f585151438dbdd
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-05-24 10:40:53 +0800

    (add):welcomne page

--- assets/js/spa/welcome.js
+++ assets/js/spa/welcome.js
@@ -1,20 +1,25 @@
 Vue.component('welcome', {
     name: 'welcome',
+    prop:['userinfo'],
     template: getWelcomeTemplate(),
     data(){
         return{
-            userInfo: this.$root.userInfo
+            userInfo:''
         }
+    },
+    mounted () {
+        this.userInfo = localStorage.getItem('userInfo');
     },
     methods: {
         changeCurrentView() {
-            if(this.userInfo){
-                console.log('home');
-                this.$emit('event','changeCurrentView','home')
-            }else{
-                console.log('login');
-                this.$emit('event','changeCurrentView','login')                
-            }
+            // if(this.userInfo){
+            //     console.log('home');
+            //     this.$emit('event','changeCurrentView','home')
+            // }else{
+            //     console.log('login');
+            //     this.$emit('event','changeCurrentView','login')                
+            // }
+            this.$emit('event','changeCurrentView','supplement')
         }
     }
 })
