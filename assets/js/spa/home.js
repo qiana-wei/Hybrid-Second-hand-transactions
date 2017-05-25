6ded1cf68ab604109732135d89fe30c4fc17448a
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-05-25 20:45:10 +0800

    (fix):get goods list

--- assets/js/spa/home.js
+++ assets/js/spa/home.js
@@ -6,6 +6,12 @@ Vue.component('home', {
             userInfo: this.$root.userInfo
         }
     },
+    mounted () {
+        let url = `/api/good/list`;
+        utils.DataService.get(url).done(res=>{
+            console.log(res);
+        })
+    },
     methods: {
         changeCurrentView(name){
             this.$emit('event','changeCurrentView',name)
