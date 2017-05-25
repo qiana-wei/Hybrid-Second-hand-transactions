64f7d665a5b5b990d58aa676bc9b6543fe8f51ab
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-05-25 20:47:00 +0800

    (fix):goods model

--- models/goods.js
+++ models/goods.js
@@ -1,18 +1,19 @@
 var mongoose = require('mongoose')
+var users = require('./users')
 
 var goodSchema = mongoose.Schema({
-    name:String,
-    img : [],
-    price : Number,
-    degree : Number,
-    type : [],
-    description : {},
-    create_time : Date,
-    view : Number,
-    like : Number,
-    sold : Boolean,
-    sold_time : Date,
-    // user_id : Schema.Types.ObjectId,
+    name:{type:String},
+    img : {type:[]},
+    price : {type:Number},
+    degree : {type:Number},
+    type : {type:[]},
+    description : {type:{}},
+    create_time : {type:Date,default:new Date()},
+    view : {type:Number,default:1},
+    like : {type:Number,default:1},
+    sold : {type:Boolean,default:false},
+    sold_time : {type:Date},
+    user_id : {type:mongoose.Schema.Types.ObjectId,ref:'users'}
 })
 
 var goodModel = mongoose.model('goods', goodSchema)
