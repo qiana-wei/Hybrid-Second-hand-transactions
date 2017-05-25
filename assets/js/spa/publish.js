cae0b53b626b3febac74e63e3c44613fdd31031e
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-05-25 20:44:31 +0800

    (fix):add publish page

--- assets/js/spa/publish.js
+++ assets/js/spa/publish.js
@@ -3,7 +3,14 @@ Vue.component('publish', {
     template: getPublishTemplate(),
     data(){
         return{
-            userInfo: this.$root.userInfo
+            userInfo: this.$root.userInfo,
+            goodsInfo:{
+                goods_name:"",
+                price : '',
+                degree : '',
+                description : ''
+            },
+            good_type : ''          
         }
     },
     mounted(){
@@ -12,6 +19,19 @@ Vue.component('publish', {
     methods: {
         changeCurrentView(name){
             this.$emit('event','changeCurrentView',name)
+        },
+        publishGoods(){
+            let url='/api/good';
+            let data=this.goodsInfo;
+            data.user_id = this.userInfo._id;
+            data.type = this.good_type.replace('\'/g','').split(' ')
+            utils.DataService.post(url,data).done(res=>{
+                this.changeGoodsId(res._id)
+                this.changeCurrentView('goods-details')
+            })
+        },
+        changeGoodsId(id){
+            this.$emit('event','changeGoodsId',id)
         }
     }
 })
