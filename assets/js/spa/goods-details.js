a4deeadce0cac901c3cdfc4b68db41cda312eaa1
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-05-25 23:32:58 +0800

    (fix):add slider

--- assets/js/spa/goods-details.js
+++ assets/js/spa/goods-details.js
@@ -14,6 +14,10 @@ Vue.component('goods-details', {
         utils.DataService.get(url).done(res=>{
             console.log(res);
         })
+        var gallery = mui('.mui-slider');
+        gallery.slider({
+            interval:0//自动轮播周期，若为0则不自动播放，默认为0；
+        });
     },
     methods: {
         changeCurrentView(name){
