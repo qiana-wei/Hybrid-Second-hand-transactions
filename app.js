b6bbc4d88545cf6a57a8d65cd1d01f99d24bbc72
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-05-25 20:39:37 +0800

    (fix):add goods api

--- app.js
+++ app.js
@@ -13,12 +13,17 @@ var db = mongoose.createConnection('localhost', 'secondhand-transformation'); //
 //router setup
 var index = require('./routes/index');
 var user_api = require('./routes/user_api');
+var good_api = require('./routes/goods_api')
 
 var userModel = require('./models/users')
 var goodModel = require('./models/goods')
 var commentModel = require('./models/comments')
 
 var app = express();
+
+//设置环境
+// app.set('env','production')
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'www'));
@@ -36,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'www')));
 //router usage setup
 app.use('/', index);
 app.use('/api/user',user_api)
+app.use('/api/good',good_api)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -59,12 +65,16 @@ var opts = {
         }
     }
 }
+
+mongoose.Promise = global.Promise
 switch (app.get('env')) {
     case 'development':
         mongoose.connect('mongodb://localhost:27017/development', opts);
+        console.log('development');
         break;
     case 'production':
-        mongoose.connect('mongodb://localhost:27017/production', opts);
+        mongoose.connect('mongodb://128.199.156.9:27017/production', opts);
+        console.log('production');
         break;
     default:
         throw new Error('Unknow execution environment:' + app.get('env'))
