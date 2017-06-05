48b7d23a0a3df5cf02c2c2c76b0a118f92d585ae
Qmengxi <mengxiaoxi_qian@sina.cn>
2017-06-05 14:07:36 +0800

    (fix):add filter

--- assets/js/spa/setup.js
+++ assets/js/spa/setup.js
@@ -1,3 +1,44 @@
+Vue.filter('time', function(value) {
+    return window.utils.formateDate(value);
+})
+Vue.filter('lowercase', function(value) {
+    if(value)
+        return value.toLowerCase();
+})
+
+Vue.filter('authority', function(value) {
+    return value ? 'Private' : 'Public';
+})
+
+Vue.filter('short_appname', function(value) {
+    return value.split(' ')[1];
+})
+Vue.filter('ago', function(value) {
+    var seconds = Math.floor((new Date() - new Date(value.replace(' ', 'T'))) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval >= 1) {
+        return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+        return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval >= 1) {
+        return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval >= 1) {
+        return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+})
 function getSTTemplate() {
     return {
         gulp_inject: '../templates/ST.html'
